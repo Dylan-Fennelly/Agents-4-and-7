171d3b6a3a70ab9a20ed01992842f77db3a70469
albertSkalinski <D00248346@student.dkit.ie>
2025-04-01 01:37:43 +0100

    :bento: Update titlescreen
    Updating the titlescreen by changing the resolution of the original file
    so it fits in the fullscreen window

--- gd4_sfml_tcp_game/Application.cpp
+++ gd4_sfml_tcp_game/Application.cpp
@@ -20,7 +20,7 @@ Application::Application()
 {
 	m_window.setKeyRepeatEnabled(false);
 	m_fonts.Load(Font::kMain, "Media/Fonts/Sansation.ttf");
-	m_textures.Load(TextureID::kTitleScreen, "Media/Textures/TitleScreen.png");
+	m_textures.Load(TextureID::kTitleScreen, "Media/Textures/TitleScreen2.png");
 	m_textures.Load(TextureID::kButtonNormal, "Media/Textures/ButtonNormal.png");
 	m_textures.Load(TextureID::kButtonSelected, "Media/Textures/ButtonSelected.png");
 	m_textures.Load(TextureID::kButtonActivated, "Media/Textures/ButtonPressed.png");
